5c04a9410624aa85b4c0dbd3acd0a844abf28a09
cdominguez_axity <cdominguez.bravo@gmail.com>
2019-10-21 15:32:16 -0300

    (27:03) Working With Strings from: https://www.youtube.com/watch?v=rfscVS0vtbw&t=3954s

--- Giraffe/app.py
+++ Giraffe/app.py
@@ -1,4 +1,12 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+from builtins import print
+
+character_name = "John"
+chracter_age = "35"
+is_male = False
+
+print("There once was a man named " + character_name + ", ")
+print("He was "+chracter_age+" years old" )
+
+character_name = "Tom"
+
+print("He really like the name  " + character_name + ", ")
